82669b3b4f932192a1b05ea3e75f4184cd0f224e
Mauricio Santaella Ochoa <mao8a87@gmail.com>
2021-12-22 00:10:20 -0500

    Update tema3.vue

--- src/views/tema3.vue
+++ src/views/tema3.vue
@@ -19,19 +19,19 @@
     #t_3_1.titulo-segundo
       h2 3.1 Diseño centrado en el usuario
     
-    p.mb-5 En esta última sección, se quiere dar a conocer una breve ilustración sobre una de las metodologías más importantes para el desarrollo de sistemas interactivos software. Esta metodología, además de ser estandarizada por la ISO 13407, propone una serie de lineamientos y fases para el éxito de las aplicaciones.
+    p.mb-5 En esta última sección, se quiere dar a conocer una breve ilustración sobre una de las metodologías más importantes para el desarrollo de sistemas interactivos <em>software</em>. Esta metodología, además de ser estandarizada por la ISO 13407, propone una serie de lineamientos y fases para el éxito de las aplicaciones.
 
     .row.align-items-center.justify-content-center.mb-3
       .col-lg-10
         h3.mb-3 ¿Por qué diseño centrado en el usuario?
-        p.mb-3 En la literatura, se encuentra una vasta información sobre las definiciones acerca de la metodología del Diseño Centrado en el Usuario <b>DCU</b> y de la experiencia de usuario UX. Por lo que, a nivel general, las definiciones convergen a que el <b>DCU</b>es un enfoque del diseño que se centra en los usuarios, a través de la planificación, el diseño y el desarrollo de un producto. Sus procesos y estrategias varían según el tipo de proyecto de diseño, no obstante, generalmente se basan en una combinación de actividades de investigación y diseño de <b>UX.</b> (Justinmind, 2018).
+        p.mb-3 En la literatura, se encuentra una vasta información sobre las definiciones acerca de la metodología del Diseño Centrado en el Usuario <b>DCU</b> y de la experiencia de usuario UX. Por lo que, a nivel general, las definiciones convergen a que el <b>DCU</b> es un enfoque del diseño que se centra en los usuarios, a través de la planificación, el diseño y el desarrollo de un producto. Sus procesos y estrategias varían según el tipo de proyecto de diseño, no obstante, generalmente se basan en una combinación de actividades de investigación y diseño de <b>UX.</b> (Justinmind, 2018).
     
     .row.align-items-center.justify-content-center.mb-3     
       .col-lg-10
         .jumbotron-13.mb-3.p-4
           .row.justify-content-center.align-items-center.sp5.p-2
             .col-lg-7
-              p El DCU es un paradigma, de cierta forma, cambia la forma en que se piensa y se percibe sobre los seres humanos y las computadoras. Este tipo de proceso de diseño tiene como objetivo involucrar activamente a los usuarios en cada etapa del proceso de construcción de un sistema interactivo <i>software</i>,, para el desarrollo de productos más efectivos, eficientes y seguros. Estas etapas incluyen:  
+              p El DCU es un paradigma, de cierta forma, cambia la forma en que se piensa y se percibe sobre los seres humanos y las computadoras. Este tipo de proceso de diseño tiene como objetivo involucrar activamente a los usuarios en cada etapa del proceso de construcción de un sistema interactivo <i>software</i>, para el desarrollo de productos más efectivos, eficientes y seguros. Estas etapas incluyen:  
             .col-lg-5.mb-5.mb-lg-0
               img(src="@/assets/curso/Images/tema3/img_2.svg", alt="")
     
@@ -49,13 +49,12 @@
           .row.align-items-center
             .col-lg-8
               h3.mb-3 Concepto y estrategia
-              p La siguiente etapa incluye la conceptualización del proyecto, que consiste en organizar el mismo de manera esquemática. Un sitio <i>web</i> puede contener varias páginas <i>web</i>, es necesario definir la cantidad antes de iniciar su desarrollo. También, el sitio <i>web</i> puede necesitar ciertas funcionalidades, como base de datos, sistemas de seguridad, entre otros aspectos importantes. Para ello, se realizan mapas del sitio o sitemaps, diseño de arquitectura de la información y categorización de productos. Esta última implica identificar la línea de productos, como sucede con los <i>e-commerce,</i> donde se puede utilizar card sorting, que es una herramienta sencilla y práctica para establecer estas categorías.
+              p La siguiente etapa incluye la conceptualización del proyecto, que consiste en organizar el mismo de manera esquemática. Un sitio <i>web</i> puede contener varias páginas <i>web</i>, es necesario definir la cantidad antes de iniciar su desarrollo. También, el sitio <i>web</i> puede necesitar ciertas funcionalidades, como base de datos, sistemas de seguridad, entre otros aspectos importantes. Para ello, se realizan mapas del sitio o <em>sitemaps</em>, diseño de arquitectura de la información y categorización de productos. Esta última implica identificar la línea de productos, como sucede con los <i>e-commerce,</i> donde se puede utilizar <em>card sorting</em>, que es una herramienta sencilla y práctica para establecer estas categorías.
             .col-lg-4
               figure
                 img(src='@/assets/curso/Images/tema3/img_4.svg' alt='')
 
         .p-4(titulo='Diseño' numero='3')
-          p El controlador no es más que el interlocutor entre la vista y el modelo, gestionando y adaptando la información entre ellas.
           .row.align-items-center
             .col-lg-8
               h3.mb-3 Diseño
@@ -121,9 +120,9 @@
         img(src="@/assets/curso/Images/tema3/img_9.svg", alt="")
     
     .row.justify-content-center.mb-5
-      h3.mb-3 SÍNTESIS
+      h3.mb-3 Síntesis
 
-      p.mb-3 Las pruebas de usabilidad son una necesidad para el desarrollador de <i>software</i>,, debe tenerse en cuenta siempre la necesidad del usuario, ya que los gustos e intereses de quien desarrolla el contenido y los de quien lo va a usar no son necesariamente correspondientes. Una vez se ha determinado cuál es la prueba que mejor se adapta a sus necesidades, se requiere encontrar participantes válidos, bien sea usuarios directos o expertos en los contenidos que se encuentra desarrollando, sin perder de vista que estos deben tener correspondencia con la población objetivo a la cual se pretende impactar.
+      p.mb-3 Las pruebas de usabilidad son una necesidad para el desarrollador de <i>software</i>, debe tenerse en cuenta siempre la necesidad del usuario, ya que los gustos e intereses de quien desarrolla el contenido y los de quien lo va a usar no son necesariamente correspondientes. Una vez se ha determinado cuál es la prueba que mejor se adapta a sus necesidades, se requiere encontrar participantes válidos, bien sea usuarios directos o expertos en los contenidos que se encuentra desarrollando, sin perder de vista que estos deben tener correspondencia con la población objetivo a la cual se pretende impactar.
 
       p.mb-3 Las diferentes pruebas que hemos revisado permiten validar la usabilidad y accesibilidad de los contenidos, a la vez que mejorarán la experiencia de los usuarios, y ahorrarán tiempo y dinero en su creación.
 
